1eef07b9f82bd8a9b439eb616177c22c4f0c30d8
Alan Alves Luiz <aaluiz@gmail.com>
2019-03-03 09:33:30 -0300

    save code

---
+++ FrontEnd/wwwroot/precache-manifest.073fecdc81ff46cbc254ec449fc22bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9aeb03a9bfe3f4cb0bc4",
+    "url": "/static/js/main.9aeb03a9.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "9aeb03a9bfe3f4cb0bc4",
+    "url": "/static/css/main.0a5d265f.chunk.css"
+  },
+  {
+    "revision": "35984aec7f5e78b43abd2a2e60df71e2",
+    "url": "/index.html"
+  }
+];
