61b6aab4cbb813078b548e0947dadf38f8cc2f42
Alan Alves Luiz <aaluiz@gmail.com>
2019-03-05 11:28:32 -0300

    raise app

---
+++ FrontEnd/_react/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';  
+import ItemsListContainer from '../containers/ItemsListContainer';
+
+const App = () => {
+    return (
+      <div>
+        <ItemsListContainer />
+      </div>
+    );
+  };
+
+  export default App;
---
+++ FrontEnd/_react/src/containers/ItemsListContainer.jsx
@@ -0,0 +1,28 @@
+import ItemsList from '../components/ItemsList';
+import addItem from '../actions/formActions/addItem';
+import deleteItem from '../actions/formActions/deleteItem';
+import {connect } from "react-redux";
+
+const mapStateToProps = state => {
+    return {
+      items: state
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      onAdd: (name, price) => {
+        dispatch(addItem(name, price));
+      },
+      onDelete: id => {
+        dispatch(deleteItem(id));
+      }
+    };
+  };
+  
+  //container components
+  const ItemsListContainer = connect(mapStateToProps, mapDispatchToProps)(
+    ItemsList
+  );
+
+  export default ItemsListContainer;
