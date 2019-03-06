384e128a65c63fce54688cc15a43e258ed2fec61
Alan Alves Luiz <aaluiz@gmail.com>
2019-03-06 09:09:32 -0300

    Integrating bootstrap with jquery on react app

--- FrontEnd/_react/src/components/App.jsx
+++ FrontEnd/_react/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';  
 import ItemsListContainer from '../containers/ItemsListContainer';
 import Navigation from '../template/Navigation';
+import Header from '../template/Header';
 
 const App = () => {
     return (
       <div>
         <Navigation />
+        <Header/>
         <ItemsListContainer />
       </div>
     );
